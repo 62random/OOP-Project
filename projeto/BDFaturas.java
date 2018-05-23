3170a80190386a795c587af4f58f49f990f15db0
62random <josepedropinto98@gmail.com>
2018-05-23 01:29:48 +0100

    ola

--- projeto/BDFaturas.java
+++ projeto/BDFaturas.java
@@ -1,14 +1,10 @@
 import java.util.Set;
 import java.util.Map;
 import java.util.HashSet;
-import java.util.TreeSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.LinkedList;
 import java.util.stream.Collectors;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.io.*;
 
 
